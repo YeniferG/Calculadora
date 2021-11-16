de32cbc7d49c6734126c4ec3b0a8f8b3f5e3c161
Yenifer <guajeyenifer@gmail.com>
2021-11-16 17:39:31 -0500

    Impresión de las operaciones de la calculadora en la clase Main

--- src/main/java/Calculadora/Main.java
+++ src/main/java/Calculadora/Main.java
@@ -1,4 +1,13 @@
 package Calculadora;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("***Bienvenido a la Calculadora Basica***");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        Menu menu = new Menu();
+        menu.cargarMenu();
+    }
+
 }
