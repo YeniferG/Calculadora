890c76118266b6dcd2f9f38137724191ce9c05d1
Yenifer <guajeyenifer@gmail.com>
2021-11-16 17:05:43 -0500

    Creacion de la estructura de la clase Operaciones

--- src/main/java/Calculadora/Operaciones.java
+++ src/main/java/Calculadora/Operaciones.java
@@ -1,4 +1,28 @@
 package Calculadora;
 
+import java.util.Scanner;
+
 public class Operaciones {
+
+    Scanner entrada = new Scanner(System.in);
+
+    private double resultado;
+    private double num1, num2;
+
+    public Operaciones(){}
+
+    public String calculadora(double num1, double num2){
+        return "El resultado de la Operación es: " + resultado;
+    }
+
+    public void iniciarCalculadora(){
+
+        System.out.println("Por favor ingrese un numero: ");
+        num1 = entrada.nextDouble();
+        System.out.println("Por favor ingrese un nuevo numero: ");
+        num2 = entrada.nextDouble();
+        System.out.println(calculadora(num1, num2));
+
+    }
+
 }
